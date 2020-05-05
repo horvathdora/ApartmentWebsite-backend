418b68a249428953d8c4219b802465e80c22affe
Dóra Horváth <horvath.dora06@gmail.com>
2020-05-05 14:55:20 +0200

    added sonar-scanner

--- src/main/java/com/reservation/security/WebSecurityConfig.java
+++ src/main/java/com/reservation/security/WebSecurityConfig.java
@@ -46,7 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("api/admin/**").hasRole("ROLE_ADMIN")
+                .antMatchers("api/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
